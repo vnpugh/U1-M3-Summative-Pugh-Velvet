0df2e9aa5a95ed3d431cfbc575b7344fd787fc33
Velvet Pugh <vnp19@yahoo.com>
2019-07-18 14:20:24 -0500

    first commit

---
+++ src/main/java/com/company/U1M3SummativePughVelvet/U1M3SummativePughVelvetApplication.java
@@ -0,0 +1,13 @@
+package com.company.U1M3SummativePughVelvet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U1M3SummativePughVelvetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U1M3SummativePughVelvetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/U1M3SummativePughVelvet/U1M3SummativePughVelvetApplicationTests.java
@@ -0,0 +1,16 @@
+package com.company.U1M3SummativePughVelvet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class U1M3SummativePughVelvetApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
